a78567ce3dffc2df98386dac307c76c8b8b02296
lgfas <luis.guilherme2811@gmail.com>
2025-03-26 20:39:28 -0300

    feat: @media on header and other fixes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -41,21 +41,22 @@ const toggleBtn = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.header-menu-nav');
 const toggleIcon = toggleBtn.querySelector('img');
 
-const iconOpen = './assets/menu-hamburguer.svg'; // ícone padrão (hambúrguer)
-const iconClose = './assets/menu-close.svg';         // ícone de fechar
+const iconOpen = './assets/menu-hamburguer.svg'; // Ícone padrão (hambúrguer)
+const iconClose = './assets/menu-close.svg';    // Ícone de fechar
 
 toggleBtn.addEventListener('click', () => {
-  menu.classList.toggle('active');
+    menu.classList.toggle('active');
 
-  // Alterna entre os ícones
-  if (menu.classList.contains('active')) {
-    toggleIcon.src = iconClose;
-    toggleBtn.setAttribute('aria-label', 'Fechar menu');
-  } else {
-    toggleIcon.src = iconOpen;
-    toggleBtn.setAttribute('aria-label', 'Abrir menu');
-  }
+    // Alterna entre os ícones
+    if (menu.classList.contains('active')) {
+        toggleIcon.src = iconClose;
+        toggleBtn.setAttribute('aria-label', 'Fechar menu');
+    } else {
+        toggleIcon.src = iconOpen;
+        toggleBtn.setAttribute('aria-label', 'Abrir menu');
+    }
 });
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const testimonials = [
